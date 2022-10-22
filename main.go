0bcd7b3fcad122b1217144f2ce9762391c2d4fd3
OmarAouini <aouini.omar93@gmail.com>
2022-10-23 01:30:46 +0200

    cleanup, gitignore

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/OmarAouini/go_tdd/config"
 	"github.com/OmarAouini/go_tdd/kafka"
 	"github.com/OmarAouini/go_tdd/logging"
@@ -17,6 +19,7 @@ func main() {
 
 	brokers := []string{"localhost:29092"}
 	kafka.SendMessage(brokers, "test_topic", Person{ID: 22, Nome: "test_name"})
+	time.Sleep(time.Second * 3)
 	kafka.ConsumeMessages(brokers, "test_topic", func(b []byte) {
 		//action here
 	})
